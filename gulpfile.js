5874d2d5f6c11c145afc451d2a42bb5e6095b8b1
PavelSlastenov <p.slastenov@mail.ru>
2020-01-15 12:15:50 +0300

    Убрал лишние строки

--- gulpfile.js
+++ gulpfile.js
@@ -67,6 +67,7 @@ gulp.task("server", function () {
   gulp.watch("source/less/**/*.less", gulp.series("css", "refresh"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
 });
 gulp.task("copy", function () {
   return gulp.src([
@@ -105,7 +106,3 @@ gulp.task("build", gulp.series(
 ));
 
 gulp.task("start", gulp.series("build", "server"));
-
-gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
-gulp.watch("source/*.html", gulp.series("html", "refresh"));
-gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
