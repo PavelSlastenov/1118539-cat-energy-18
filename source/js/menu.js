f111c17145a3958d02d517c254118dd93a9f76ef
PavelSlastenov <p.slastenov@mail.ru>
2020-01-22 17:33:45 +0300

    Реализовал открытое меню без JS

--- source/js/menu.js
+++ source/js/menu.js
@@ -4,7 +4,7 @@ var navToggle = document.querySelector(".main-nav__toggle");
 navMain.classList.remove("main-nav--opened");
 navMain.classList.add("main-nav--closed");
 
-navToggle.addEventListener('click', function() {
+navToggle.addEventListener("click", function() {
   if (navMain.classList.contains("main-nav--closed")) {
     navMain.classList.remove("main-nav--closed");
     navMain.classList.add("main-nav--opened");
